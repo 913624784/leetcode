a52c86e42cb11f0552ee66bafbbfded552d6a746
qizhiwei <qizhiwei@meituan.com>
2022-02-09 20:44:20 +0800

    add test demo

---
+++ src/main/java/com/github/qzw/dynamic_programming/_2_背包问题.java
@@ -0,0 +1,62 @@
+package com.github.qzw.dynamic_programming;
+
+import java.util.Arrays;
+
+/**
+ * @Author: qizhiwei
+ * @date: 2022/2/9
+ * @PackageName: com.github.qzw.dynamic_programming
+ * @Description: 0-1背包
+ * 问题：给你一个可放总重量为 W 的背包和 N 个物品，对每个物品，有重量 w 和价值 v 两个属性，那么第 i 个物品的重量为 w[i]，价值为 v[i]。现在让你用这个背包装物品
+ * 问最多能装的价值是多少？
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：W = 5, N = 3
+ * w = [3, 2, 1], v = [5, 2, 3]
+ * 输出：8
+ * 解释：选择 i=0 和 i=2 这两件物品装进背包。它们的总重量 4 小于 W，同时可以获得最大价值 8
+ */
+public class _2_背包问题 {
+
+    /**
+     * @param w 物品的重量
+     * @param v 物品的价值
+     * @param N 物品的总数
+     * @param W 背包能容纳的总重量
+     * @return 最大价值
+     */
+    static int dp(int[] w, int[] v, int N, int W) {
+        // 创建备忘录，初始化状态
+        int[][] dp = new int[N + 1][W + 1];
+
+        /*
+         * tn: traversed n，即已经遍历过的物品
+         * rw: reserved w，即背包还能容量的重量
+         */
+        for (int tn = 1; tn < N + 1; tn++) { // 遍历每一件物品
+            for (int rw = 1; rw < W + 1; rw++) { // 背包容量有多大就还要计算多少次
+                if (rw < w[tn - 1]) {
+                    // 当背包容量小于第tn件物品重量时，使用上一个物品的最大价值
+                    dp[tn][rw] = dp[tn - 1][rw];
+                } else {
+                    // 当背包容量还大于第tn件物品重量时，进一步作出决策
+                    // a. 放入这个物品前的最大价值 + 当前物品价值和作为答案
+                    // b. 不放入这个物品时，当前容量的最大价值作为答案
+                    dp[tn][rw] = Math.max(dp[tn - 1][rw], dp[tn - 1][rw - w[tn - 1]] + v[tn - 1]);
+                }
+            }
+        }
+        for (int[] x : dp) {
+            System.out.println(Arrays.toString(Arrays.stream(x).toArray()));
+        }
+        return dp[N][W];
+    }
+
+    public static void main(String[] args) {
+        int N = 3, W = 5; // 物品的总数，背包能容纳的总重量
+        int[] w = {3, 2, 1}; // 物品的重量
+        int[] v = {5, 2, 3}; // 物品的价值
+        System.out.println(dp(w, v, N, W));
+    }
+}
---
+++ src/main/java/com/github/qzw/dynamic_programming/_3_背包问题延伸_最后一块石头的重量2.java
@@ -0,0 +1,59 @@
+package com.github.qzw.dynamic_programming;
+
+import java.util.Arrays;
+
+/**
+ * @Author: qizhiwei
+ * @date: 2022/2/9
+ * @PackageName: com.github.qzw.dynamic_programming
+ * @Description: 背包问题延伸
+ * 问题：有一堆石头，每块石头的重量都是正整数。每次从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x ≤ y。那么粉碎的可能结果如下：
+ * <p>
+ * 如果 x 与 y 相等，那么两块石头都会被完全粉碎；
+ * 否则，重量为 x 的石头将会完全粉碎，而重量为 y 的石头的新重量为 y - x。
+ * 最后，最多只会剩下一块石头。返回此时石头最小的可能重量。如果没有石头剩下，就返回 0。
+ * <p>
+ * 示例：
+ * 输入：[1, 2, 1, 7, 9, 4]
+ * 输出：
+ * Round 1: (2, 4) -> 2, 数组变成 [1, 1, 7, 9, 2]
+ * Round 2: (7, 9) -> 2, 数组变成 [1, 1, 2, 2]
+ * Round 3: (2, 2) -> 0, 数组变成 [1, 1]
+ * Round 4: (1, 1) -> 0, 数组为空，返回 0
+ * Tips：
+ * 现在，假设有一个背包，背包的容量是 12（24/2）。接着，我们有一堆的物品，重量分别是 [1, 2, 1, 7, 9, 4]，注意我们设它的价值与重量相同。
+ * 现在我们希望选出的物品放到背包里的价值最大，这样一来，我们就可以把这个题目转化成0-1背包问题了
+ */
+public class _3_背包问题延伸_最后一块石头的重量2 {
+    static int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int sum = Arrays.stream(stones).sum();
+
+        //背包容量上限为石头总重量的一半
+        int[][] dp = new int[n + 1][sum / 2 + 1];
+        /*
+         * tn: traversed n，即已经遍历过的石头
+         * rw: reserved w，即背包还能容量的重量
+         */
+        for (int tn = 1; tn < n + 1; tn++) {
+            for (int rw = 1; rw < sum / 2 + 1; rw++) {
+                if (rw < stones[tn - 1]) {
+                    //这块石头放不进背包时，只能跳过
+                    dp[tn][rw] = dp[tn - 1][rw];
+                } else {
+                    //当我能把这块石头放进背包时，我会比较放或不放，选择最大值
+                    dp[tn][rw] = Math.max(dp[tn - 1][rw], dp[tn - 1][rw - stones[tn - 1]] + stones[tn - 1]);
+                }
+            }
+        }
+        for (int[] x : dp) {
+            System.out.println(Arrays.toString(Arrays.stream(x).toArray()));
+        }
+        return sum - dp[n][sum / 2] * 2;
+    }
+
+    public static void main(String[] args) {
+        int[] stones = {1, 2, 1, 7, 9, 4};
+        System.out.println(lastStoneWeightII(stones));
+    }
+}
